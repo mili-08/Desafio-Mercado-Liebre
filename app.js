52deeae0596df3c7862f5b82ef51cc227494d234
mili-08 <milijmartinez@gmail.com>
2023-08-13 17:19:18 -0300

    Se modifica la estructura del home.html

--- app.js
+++ app.js
@@ -16,33 +16,16 @@ app.get('/login.html',(req,res)=> {
     res.sendFile(__dirname + '/views/login.html')
 });
 
-
-app.get('/ofertas.html',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/ofertas.html'));
-});
-
 app.get('/tiendasOficales.html',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views/tiendasOficales.html'));
+    res.sendFile(__dirname + '/views/tiendasOficales.html');
 });
 
 app.get('/vender.html',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views/vender.html'));
-});
-
-app.get('/ayuda.html',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views/ayuda.html'));
-});
-
-app.get('/cuenta.html',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views/cuenta.html'));
-});
-
-app.get('/ingresar.html',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views/ingresar.html'));
+    res.sendFile(__dirname + '/views/vender.html');
 });
 
 app.get('/misCompras.html',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views/misCompras.html'));
+    res.sendFile(__dirname + '/views/misCompras.html');
 });
 
 
